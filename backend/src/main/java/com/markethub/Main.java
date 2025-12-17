d6c191212ef7ca3e50644882f3ad0d5e9ffe9c4d
Yugi <yugidr0@gmail.com>
2025-12-16 19:26:28 -0600

    feat: setup spring boot app and optimize docker config

--- backend/src/main/java/com/markethub/Main.java
+++ backend/src/main/java/com/markethub/Main.java
@@ -1,7 +1,20 @@
 package com.markethub;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello everyone!");
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello";
     }
 }
