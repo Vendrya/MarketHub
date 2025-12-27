40dc903a22aa55959ddb81acab9be06bae29b58a
eldanielhumberto <gamekik92@gmail.com>
2025-12-27 00:04:49 -0600

    refactor: move setTokenCookies function to AuthService

--- backend/src/main/java/com/markethub/features/auth/AuthController.java
+++ backend/src/main/java/com/markethub/features/auth/AuthController.java
@@ -8,9 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,19 +17,13 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @Value("${application.security.jwt.expiration}")
-    private long jwtExpiration;
-
-    @Value("${application.security.jwt.refresh-token.expiration}")
-    private long refreshExpiration;
-
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(
             @Valid @RequestBody RegisterRequest request,
             HttpServletResponse response
     ) {
         AuthResponse authResponse = authService.register(request);
-        setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
+        authService.setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
 
         authResponse.setAccessToken(null);
         authResponse.setRefreshToken(null);
@@ -46,7 +37,7 @@ public class AuthController {
             HttpServletResponse response
     ) {
         AuthResponse authResponse = authService.login(request);
-        setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
+        authService.setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
 
         authResponse.setAccessToken(null);
         authResponse.setRefreshToken(null);
@@ -73,7 +64,7 @@ public class AuthController {
 
         AuthResponse authResponse = authService.refreshToken(refreshToken);
 
-        setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
+        authService.setTokenCookies(response, authResponse.getAccessToken(), authResponse.getRefreshToken());
 
         authResponse.setAccessToken(null);
         authResponse.setRefreshToken(null);
@@ -81,25 +72,5 @@ public class AuthController {
         return ResponseEntity.ok(authResponse);
     }
 
-    private void setTokenCookies(HttpServletResponse response, String accessToken, String refreshToken) {
-        ResponseCookie accessCookie = ResponseCookie.from("access_token", accessToken)
-                .httpOnly(true)
-                .secure(false)        // 'true' for HTTPS in production
-                .path("/")
-                .maxAge(jwtExpiration / 1000)
-                .sameSite("Strict")
-                .build();
 
-        // Cookie de Refresh Token
-        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", refreshToken)
-                .httpOnly(true)
-                .secure(false)
-                .path("/")
-                .maxAge(refreshExpiration / 1000)
-                .sameSite("Strict")
-                .build();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
-        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
-    }
 }
--- backend/src/main/java/com/markethub/features/auth/AuthService.java
+++ backend/src/main/java/com/markethub/features/auth/AuthService.java
@@ -7,7 +7,11 @@ import com.markethub.features.user.models.Role;
 import com.markethub.features.user.models.User;
 import com.markethub.features.user.repository.UserRepository;
 import com.markethub.security.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,11 +20,16 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-    private final JwtService jwtService;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
     private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
 
     public AuthResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -78,5 +87,27 @@ public class AuthService {
             }
         }
         throw new IllegalArgumentException("Token expired or invalid");
+    }
+
+    public void setTokenCookies(HttpServletResponse response, String accessToken, String refreshToken) {
+        ResponseCookie accessCookie = ResponseCookie.from("access_token", accessToken)
+                .httpOnly(true)
+                .secure(false)        // TODO: use environment system for set 'true' in production environment or 'false' in dev environment
+                .path("/")
+                .maxAge(jwtExpiration / 1000)
+                .sameSite("Strict")
+                .build();
+
+        // Cookie de Refresh Token
+        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", refreshToken)
+                .httpOnly(true)
+                .secure(false)
+                .path("/")
+                .maxAge(refreshExpiration / 1000)
+                .sameSite("Strict")
+                .build();
+
+        response.addHeader(HttpHeaders.SET_COOKIE, accessCookie.toString());
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
     }
 }
